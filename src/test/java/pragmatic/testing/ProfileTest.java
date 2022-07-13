0fb087b0a004c8a03ceb0264f6a3f397b3c92e58
Igor Shmagrinskiy <igor.shmagrinsky@axibase.com>
2022-07-13 19:25:01 +0300

    extract arrange to create method

--- src/test/java/pragmatic/testing/ProfileTest.java
+++ src/test/java/pragmatic/testing/ProfileTest.java
@@ -1,38 +1,46 @@
 package pragmatic.testing;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
-    @Test
-    public void matchAnswersTrueForAnyDontCareCriteria() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        BaseQuestion question = new BooleanQuestion(1, "Got milk?");
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DONT_CARE);
-        criteria.add(criterion);
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
 
-        boolean matches = profile.matches(criteria);
-        assertTrue(matches);
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
     }
 
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        BaseQuestion question = new BooleanQuestion(1, "Got bonuses?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MUST_MATCH);
         criteria.add(criterion);
 
         boolean matches = profile.matches(criteria);
+
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DONT_CARE);
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
     }
 }
